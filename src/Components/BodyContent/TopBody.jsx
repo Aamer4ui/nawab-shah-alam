83b5d84674fcacd1b95f7c00d493a7f374645053
Aamer4ui <aamersohelansari@gmail.com>
2021-02-08 16:10:49 +0530

    adding main body

--- src/Components/BodyContent/TopBody.jsx
+++ src/Components/BodyContent/TopBody.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Carousel } from "react-bootstrap";
 
 export const TopBody = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block" src="1.jpg" alt="" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block" src="2.jpg" alt="" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block" src="3jpg.jpg" alt="" />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
-
