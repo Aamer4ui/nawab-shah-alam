0eac35e0534833a5f84d300958607013ead51036
Aamer4ui <aamersohelansari@gmail.com>
2021-02-09 00:16:58 +0530

    card information first

--- src/Components/BodyContent/SecondBody/SecondBodyContent.jsx
+++ src/Components/BodyContent/SecondBody/SecondBodyContent.jsx
@@ -9,9 +9,10 @@ export const SecondBodyContent = () => {
         <div class="text">
           <h3>Nawab Shah Alam</h3>
           <p class="para">
-            Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta
-            bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto
-            kevin turducken.
+            This institution has been established under the aegis of
+            MADARSA-I-AIZZA, a registered educational society registered on 28th
+            December 1961 under the Hyderabad societies registration Act No. 1
+            of 1350 fosli.
           </p>
         </div>
       </div>
