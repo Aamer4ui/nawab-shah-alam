da27e48298a77a585c920650a1da9c7f5700ab8b
Aamer4ui <aamersohelansari@gmail.com>
2021-02-09 00:52:09 +0530

    Adding Card Info

--- src/Components/BodyContent/CardContent/CardData.jsx
+++ src/Components/BodyContent/CardContent/CardData.jsx
@@ -4,16 +4,56 @@ import { Card } from "react-bootstrap";
 
 export const CardData = () => {
   return (
-    <div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+    <div className="college-details">
+      <Card className="content" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="card4.jpg" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Conference Hall</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <a href="">See More..</a>
+        </Card.Body>
+      </Card>
+
+      {/* -----------Second Card----------------- */}
+      <Card className="content" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="card2.jpg" />
+        <Card.Body>
+          <Card.Title>Playing Hall</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <a href="">See More..</a>
+        </Card.Body>
+      </Card>
+
+      {/* ------------third Card--------------------- */}
+
+      <Card className="content" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="card3.jpg" />
+        <Card.Body>
+          <Card.Title>Meeting Room</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <a href="">See More..</a>
+        </Card.Body>
+      </Card>
+
+      {/* ----------------Fouth Card------------------ */}
+      <Card className="content" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="card3.jpg" />
+        <Card.Body>
+          <Card.Title>Computer Labs</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <a href="">See More..</a>
         </Card.Body>
       </Card>
     </div>
